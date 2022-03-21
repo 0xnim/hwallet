8d67a2745e9d8aaaa544923bd0c246bfe3f17bd5
Niklas Wojtkowiak <niklas@wojtkowiak.com>
2022-03-21 07:45:49 +0200

    a

---
+++ run.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import json
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/<name>/<currency>")
+def profile(name, currency):
+    response1 = requests.get("https://api.niami.io/hsd/"+name)
+    response = response1.json()
+    address = None
+    if response["success"] == True:
+        data = response["data"]
+        if name == data["name"]:
+            dnsdata = data["dnsData"]
+            for i in dnsdata:
+                if i["type"] == "TXT":
+                    long = i['txt']
+                    if long[0].startswith("profile") == True:
+                        wallet = long[0].replace("profile", "")
+                        if "wallet" in wallet:
+                            wallet = wallet.replace("wallet=", "")
+                            wallet = wallet.replace(" ", "")
+                            if wallet.startswith(currency):
+                                address = wallet.replace(currency, "")
+    return address
